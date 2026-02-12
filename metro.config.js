b8814a5a8361a4c1f447e2f188716bcb4cbcc9d5
ThermalEcho <quantumbyte404@proton.me>
2026-02-12 10:27:02 +0000

    adwa

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withUniwindConfig } = require('uniwind/metro');
-
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-
-// adicionar extensão sql
 config.resolver.sourceExts.push('sql');
-
-module.exports = withUniwindConfig(config, {
-  cssEntryFile: './global.css',
-  dtsFile: './uniwind-types.d.ts',
-});
+module.exports = config;
